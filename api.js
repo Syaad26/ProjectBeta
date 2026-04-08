29b84e0d8db95418fdd64672daee3ff2e059f480
Muhammad Izzudin Irsyad <225443018@mhs.polman-bandung.ac.id>
2026-04-08 22:15:18 +0700

    add railway domain to api file

--- api.js
+++ api.js
@@ -2,7 +2,7 @@
 //  API CLIENT — komunikasi dengan backend server
 // ============================================================
 
-const API_URL = "http://localhost:3000/api";
+const API_URL = "libraryosjs-production.up.railway.app/api";
 
 async function apiCall(endpoint, method = "GET", data = null) {
   const options = {
